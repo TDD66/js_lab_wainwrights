69be02c991f5c3d0cedce307784ab53ef3482fa2
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-03-19 16:26:30 +0000

    Completed task 3

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
-let jsonData;
 const wainwrightsList = document.getElementById("wainwrights-list");
 
-fetchWainwrights = async () => {
+const getAllWainwrights = async (filter) => {
     const response = await fetch("https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json");
-    jsonData = await response.json();
-    return jsonData;
-}
-
-const getAllWainwrights = () => {
-    jsonData = fetchWainwrights();
+    const wainwrights = await response.json();
     
-    const wainwrightContainer = document.createElement("li");
-
+    wainwrights.forEach(wainwright => {
+        const listItem = createWainwrightContainer(wainwright);
+        wainwrightsList.appendChild(listItem);
+    })
 }
 
-getAllWainwrights();
-console.log(jsonData);
+const main = async () => {
+    getAllWainwrights();
+}
+main();
+
+const createWainwrightContainer = (wainwright) => {
+    const wainwrightListItem = document.createElement("li");
+
+    const wainwrightName = document.createElement("h1");
+    wainwrightName.innerText = wainwright["name"];
+
+    const wainwrightHeight = document.createElement("p");
+    wainwrightHeight.innerText = `Height (m): ${wainwright["heightMetres"]}`;
+
+    const wainwrightArea = document.createElement("ul");
+
+    for(const key in wainwright["area"]){
+        const subListItem = document.createElement("li");
+        subListItem.innerText = `${key} : ${wainwright["area"][key]}`;
+        wainwrightArea.appendChild(subListItem);
+    }
+
+    wainwrightListItem.appendChild(wainwrightName);
+    wainwrightListItem.appendChild(wainwrightHeight);
+    wainwrightListItem.appendChild(wainwrightArea);
+    return wainwrightListItem;
+}
